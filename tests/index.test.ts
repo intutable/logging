5a3886560190dc48c0f5b7b764d8bb091bf2e0c0
Martin K <mk_alternativ@posteo.de>
2021-12-05 17:08:24 +0100

    test: console-logging with 2 different notifications

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,9 +3,13 @@ import {Core, CoreNotification} from "@intutable/core"
 
 const PLUGIN_PATH = path.join(__dirname, "../")
 
-const notification: CoreNotification = { 
-    channel: "channel", method: "method", param1: "this is param1",  param2: "this is param2"
-};
+const notification1: CoreNotification = { 
+    channel: "channel1", method: "method1", param: "this is param"
+}
+
+const notification2: CoreNotification = { 
+    channel: "channel2", method: "method2", param1: "this is param1",  param2: "this is param2"
+}
 
 let core: Core
 
@@ -13,26 +17,36 @@ beforeAll(async () => {
     core = await Core.create([PLUGIN_PATH])
 
     // dummy handler to avoid undefinded-notification-handler exception
-    core.events.listenForNotifications(notification.channel, notification.method, 
-                                        (notification: CoreNotification) => {})
+    core.events.listenForNotifications(notification1.channel, notification1.method, 
+        (notification: CoreNotification) => {})
+    core.events.listenForNotifications(notification2.channel, notification2.method, 
+        (notification: CoreNotification) => {})
 })
 
 afterAll(async () => {
     await core.plugins.closeAll()
 })
 
+function loggedMassageValid(massage: string, notification: CoreNotification) {
+    const loggedJSON = JSON.parse(massage)
+    expect(Date.parse(loggedJSON.date)).not.toBeNaN()
+    expect(loggedJSON.notification).toEqual(notification)
+}
+
 describe("test logging", () => {
     
     test("log console", async () => {
         const consoleSpy = jest.spyOn(console, 'log')
 
-        await core.events.notify(notification)
+        await core.events.notify(notification1)
+        await core.events.notify(notification2)
         
-        expect(consoleSpy).toHaveBeenCalled()
-        const loggedMessage = consoleSpy.mock.calls[0][0]
-        const loggedJSON = JSON.parse(loggedMessage)
-        expect(Date.parse(loggedJSON.date)).not.toBeNaN()
-        expect(loggedJSON.notification).toEqual(notification)
+        expect(consoleSpy).toHaveBeenCalledTimes(2)
+
+        const loggedMessage1 = consoleSpy.mock.calls[0][0]
+        const loggedMessage2 = consoleSpy.mock.calls[1][0]
+        loggedMassageValid(loggedMessage1, notification1)
+        loggedMassageValid(loggedMessage2, notification2)
     })
 
 })
