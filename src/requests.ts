e01c74f69232a58641210b702f29c18ec9904f3f
Sören Ducati <Sren Ducati soeren.ducati@gmail.com>
2022-03-14 10:21:27 +0000

    chore: Added README.md, and jsdoc comments for the code.

--- src/requests.ts
+++ src/requests.ts
@@ -1,5 +1,15 @@
+/**
+ * Contains the requests of the logging plugin.
+ * @module
+ */
+
 import { CoreRequest } from "@intutable/core"
 
+/**
+ * Creates a prepared CoreRequest to access the event log.
+ * @param numberOfEvents specifies the number of events to return.
+ * @returns {CoreRequest} the prepared CoreRequest
+ */
 export function getEventsLog(numberOfEvents: number): CoreRequest {
     return {
         channel: "logging",
